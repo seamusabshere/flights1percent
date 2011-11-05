d00f29dca3d058d64769b0430b72f685fbba2162
Seamus Abshere <seamus@abshere.net>
2011-11-05 12:31:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flights1percent::Application.config.secret_token = '20bf53220781f6776fa262afcfccdc5bcf3a51d80a128e979506b5fc63bff39cbf64696728c811346231b624dd44435afab26eee92d2d92d66ee0ef78e16ec09'
